b755ec2f40fb17f82dcd3c0d47d04e0ef734fdac
20061920 <20061920@student.westernsydney.edu.au>
2026-09-22 22:44:34 +1000

    fix lint error

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -2,7 +2,6 @@ import { PrismaClient } from "@prisma/client";
 import { env } from "@repo/env/web";
 
 declare global {
-  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
